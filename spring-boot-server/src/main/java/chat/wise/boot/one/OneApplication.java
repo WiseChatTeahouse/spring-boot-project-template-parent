ff06f61e2ba39384406df5594fae1d14fc653177
siberia.hu <WiseChatTeahouse@gmail.com>
2024-06-21 15:47:29 +0800

    feat:spring-boot单体项目基座搭建

---
+++ spring-boot-server/src/main/java/chat/wise/boot/one/OneApplication.java
@@ -0,0 +1,16 @@
+package chat.wise.boot.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author siberia.hu
+ * @Package chat.wise.boot.one
+ * @Date 2024/6/21 15:35
+ */
+@SpringBootApplication
+public class OneApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OneApplication.class, args);
+    }
+}
